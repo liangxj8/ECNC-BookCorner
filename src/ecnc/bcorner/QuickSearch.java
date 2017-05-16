a17147a871003adcb1566e2221bdae90721fb7eb
liangxj8 <liangxj8@gmail.com>
2017-05-17 00:06:59 +0800

    Update
    add privilege

--- src/ecnc/bcorner/QuickSearch.java
+++ src/ecnc/bcorner/QuickSearch.java
@@ -9,13 +9,15 @@ import java.sql.*;
 public class QuickSearch {
   String SQLstring;
   String tableResult;
+  String privilege;
 
   public QuickSearch() {
     tableResult = "";
+    privilege = "普通用户";
   }
 
-  public String getSQLstring() {
-    return SQLstring;
+  public void setPrivilege(String privilege) {
+    this.privilege = privilege;
   }
 
   public void setSQLstring(String SQLstring) {
@@ -40,27 +42,49 @@ public class QuickSearch {
       result = sql.executeQuery(SQLstring);
       tableResult += "<table class=\"table table-striped\"><thead><tr>";
       tableResult += "<th style='text-align: center'>编号</th>";
-      tableResult += "<th style='text-align: center'>ISBN</th>";
+      if ("普通用户".equals(privilege)) {
+        tableResult += "<th style='text-align: center'>ISBN</th>";
+      }
       tableResult += "<th style='text-align: center'>书名</th>";
       tableResult += "<th style='text-align: center'>作者</th>";
-      tableResult += "<th style='text-align: center'>出版社</th>";
+      if ("普通用户".equals(privilege)) {
+        tableResult += "<th style='text-align: center'>出版社</th>";
+      }
       tableResult += "<th style='text-align: center'>状态</th>";
+      if ("管理员".equals(privilege)) {
+        tableResult += "<th style='text-align: center'>操作</th>";
+      }
       tableResult += "</tr></thead><tbody>";
       if (result.next()) {
-        if (result.getInt(6) == 1) {
+        if (result.getInt(6) == 1 || "管理员".equals(privilege)) {
           tableResult += "<tr>";
         } else {
           tableResult += "<tr class=\"danger\">";
         }
         tableResult += "<td style='text-align: center'>" + result.getString(1) + "</td>";
-        tableResult += "<td style='text-align: center'>" + result.getString(2) + "</td>";
+        if ("普通用户".equals(privilege)) {
+          tableResult += "<td style='text-align: center'>" + result.getString(2) + "</td>";
+        }
         tableResult += "<td style='text-align: center'>" + result.getString(3) + "</td>";
         tableResult += "<td style='text-align: center'>" + result.getString(4) + "</td>";
-        tableResult += "<td style='text-align: center'>" + result.getString(5) + "</td>";
+        if ("普通用户".equals(privilege)) {
+          tableResult += "<td style='text-align: center'>" + result.getString(5) + "</td>";
+        }
         if (result.getInt(6) == 1) {
           tableResult += "<td style='text-align: center'>在架上</td>";
+          if ("管理员".equals(privilege)) {
+            tableResult += "<td style='text-align: center'><button type=\"button\"";
+            tableResult += "class=\"btn btn-primary\"><a href=\"/bcorner/borrow.jsp?bid=";
+            tableResult += result.getString(1) + "&bookName=" + result.getString(3) + "\" style='color: #ffffff'>";
+            tableResult += "借书</a></button></td>";
+          }
         } else {
           tableResult += "<td style='text-align: center'>已借出</td>";
+          if ("管理员".equals(privilege)) {
+
+            tableResult += "<td style='text-align: center'><button type=\"button\"";
+            tableResult += "class=\"btn btn-warning\" disabled>借书</button></td>";
+          }
         }
         tableResult += "</tr>";
       } else {
@@ -68,20 +92,35 @@ public class QuickSearch {
         tableResult += "未找到相关书目</td><td></td><td></td><td></td></tr>";
       }
       while (result.next()) {
-        if (result.getInt(6) == 1) {
+        if (result.getInt(6) == 1 || "管理员".equals(privilege)) {
           tableResult += "<tr>";
         } else {
           tableResult += "<tr class=\"danger\">";
         }
         tableResult += "<td style='text-align: center'>" + result.getString(1) + "</td>";
-        tableResult += "<td style='text-align: center'>" + result.getString(2) + "</td>";
+        if ("普通用户".equals(privilege)) {
+          tableResult += "<td style='text-align: center'>" + result.getString(2) + "</td>";
+        }
         tableResult += "<td style='text-align: center'>" + result.getString(3) + "</td>";
         tableResult += "<td style='text-align: center'>" + result.getString(4) + "</td>";
-        tableResult += "<td style='text-align: center'>" + result.getString(5) + "</td>";
+        if ("普通用户".equals(privilege)) {
+          tableResult += "<td style='text-align: center'>" + result.getString(5) + "</td>";
+        }
         if (result.getInt(6) == 1) {
           tableResult += "<td style='text-align: center'>在架上</td>";
+          if ("管理员".equals(privilege)) {
+            tableResult += "<td style='text-align: center'><button type=\"button\"";
+            tableResult += "class=\"btn btn-primary\"><a href=\"/bcorner/borrow.jsp?bid=";
+            tableResult += result.getString(1) + "&bookName=" + result.getString(3) + "\" style='color: #ffffff'>";
+            tableResult += "借书</a></button></td>";
+          }
         } else {
           tableResult += "<td style='text-align: center'>已借出</td>";
+          if ("管理员".equals(privilege)) {
+
+            tableResult += "<td style='text-align: center'><button type=\"button\"";
+            tableResult += "class=\"btn btn-warning\" disabled>借书</button></td>";
+          }
         }
         tableResult += "</tr>";
       }
